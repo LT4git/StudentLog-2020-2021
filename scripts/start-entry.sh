775368716bb68e75d0ef641bfaab473b5f5c0a22
LT4git <ltaylor2023@chadwickschool.org>
2020-09-26 22:10:18 -0700

    Changed "code FILENAME" to "code $FILENAME"

--- scripts/start-entry.sh
+++ scripts/start-entry.sh
@@ -3,4 +3,4 @@ FILENAME= $(echo "logs/$(date +'%Y-%m-%d').log.md")
 touch $FILENAME
 echo "# $(date +'%A, %B %e, %Y %I:%M %p')" >> $FILENAME
 echo "- [ ]" >> $FILENAME
-code FILENAME
+code $FILENAME
